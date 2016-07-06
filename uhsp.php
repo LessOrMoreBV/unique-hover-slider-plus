ae1c5b78526ee64c50e894db736e8cc951f168af
Darren Rooijakkers <darren@lessormore.nl>
2016-07-06 15:05:45 +0200

    Initial commit.

---
+++ uhsp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Unique Hover Slider Plus
+ * @version 0.1.0
+ */
+/*
+Plugin Name: Unique Hover Slider Plus
+Plugin URI: http://www.lessormore.nl
+Description: A slider with a unique hover.
+Author: Less or More
+Author URI: http://www.lessormore.nl
+Version: 0.1.0
+License: GNU General Public License
+License URI: licence/GPL.txt
+*/
+class UniqueHoverSliderPlus
+{
+    // ...
+}
