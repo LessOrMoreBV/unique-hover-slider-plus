16cccea442900e8c66a19b5c45d38e6474005547
Darren Rooijakkers <darren@lessormore.nl>
2016-07-22 15:03:11 +0200

    Visual composer options.

--- framework/shortcodes/Slider.php
+++ framework/shortcodes/Slider.php
@@ -24,70 +24,22 @@ class Slider extends Shortcode
      * Options to map the shortcode to visual composer.
      * @var array
      */
-    // public $vc_options = [
-    //     'name' => 'Before & After Comparator',
-    //     'base' => 'comparator',
-    //     'class' => '',
-    //     'category' => 'Content',
-    //     'params' => [
-    //         [
-    //             'type' => 'attach_image',
-    //             'heading' => 'Before Image',
-    //             'param_name' => 'before_image',
-    //             'value' => '',
-    //             'description' => 'The image that will be visible from the top of the comparator.',
-    //         ],
-    //         [
-    //             'type' => 'textfield',
-    //             'heading' => 'Before Image alt text',
-    //             'param_name' => 'before_image_alt',
-    //             'value' => 'Before image.',
-    //             'description' => 'The alt text for the before image.',
-    //         ],
-    //         [
-    //             'type' => 'attach_image',
-    //             'heading' => 'After Image',
-    //             'param_name' => 'after_image',
-    //             'value' => '',
-    //             'description' => 'The image that will be visible from the bottom of the comparator.',
-    //         ],
-    //         [
-    //             'type' => 'textfield',
-    //             'heading' => 'After Image alt text',
-    //             'param_name' => 'after_image_alt',
-    //             'value' => 'After image.',
-    //             'description' => 'The alt text for the after image.',
-    //         ],
-    //         [
-    //             'type' => 'colorpicker',
-    //             'heading' => 'Handle Arrow Color',
-    //             'param_name' => 'arrow_color',
-    //             'value' => '#000000',
-    //             'description' => 'The color of the arrows within the handle.',
-    //         ],
-    //         [
-    //             'type' => 'colorpicker',
-    //             'heading' => 'Handle Background Color',
-    //             'param_name' => 'arrow_background_color',
-    //             'value' => '#FFFFFF',
-    //             'description' => 'The handle background color.',
-    //         ],
-    //         [
-    //             'type' => 'dropdown',
-    //             'heading' => 'Device',
-    //             'param_name' => 'device',
-    //             'value' => [
-    //                 'Browser' => 'browser',
-    //                 'Tablet' => 'tablet',
-    //                 'Mobile' => 'mobile',
-    //                 'None' => 'none',
-    //             ],
-    //             'std' => 'Browser',
-    //             'description' => 'Pick a device to display around the comparator.',
-    //         ],
+    public $vc_options = [
+        'name' => 'Unique Hover Slider Plus',
+        'base' => 'uhsp',
+        'class' => '',
+        'category' => 'Content',
+        'params' => [
+            [
+                'type' => 'textfield',
+                'heading' => 'Slider',
+                'param_name' => 'id',
+                'value' => '',
+                'description' => 'The id of the slider to insert. Create your own slider in the "UHSP Slider" section of the menu.',
+            ],
 
-    //     ]
-    // ];
+        ]
+    ];
 
     /**
      * Renders the comparator as HTML.
